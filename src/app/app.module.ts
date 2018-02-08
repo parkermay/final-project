7863b099b07688b8fdd32df48d869d90435893d1
parkermay <g.parker.may@gmail.com>
2018-02-07 20:45:21 -0500

    Moved all logic to app component to slim down example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { PredictComponent } from './predict/predict.component';
 import { EstimateService } from './services/estimate.service';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        PredictComponent
+        AppComponent
     ],
     imports: [
         BrowserModule,
