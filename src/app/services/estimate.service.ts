ff223a958ce3fdb79bf419cbc2b5f3fc74af18bb
parkermay <g.parker.may@gmail.com>
2018-02-07 20:21:58 -0500

    Switched from medicare data to wine ratings

--- src/app/services/estimate.service.ts
+++ src/app/services/estimate.service.ts
@@ -6,16 +6,14 @@ import { Estimate } from '../models/estimate.model';
 
 @Injectable()
 export class EstimateService {
-//    azureMlUrl = 'https://ussouthcentral.services.azureml.net/workspaces/3ada77bdf12a4bb3aad385165f1680cf/services/ab8f67e1e16e4fc496b512ae3af31dc8/execute?api-version=2.0&details=true';
-    apiMgmtUrl = 'https://pmay-final-apim.azure-api.net/final-project/estimate';
-    apiMachineLearningKey = 'Z21yGPuLBytgNtKQl5grucbM1WukVEfh8sJjo3mQ022zB/JgoSswN3yNaOiu4tOMIJ5B5K1hYUMikRiP1Z2m5A==';
-    apiMgmtKey = '8dce00104ad743ff8574870671589bac';
+//    azureMlUrl = 'xxxx';
+    apiMgmtUrl = 'xxxx';
+    apiMachineLearningKey = 'xxxx';
+    apiMgmtKey = 'xxxx';
 
     constructor(private http: HttpClient) { }
 
-    calculateEstimate(procedureName: string, hospitalRegion: string): Observable<Estimate> {
-        console.log(' procedureName = ' + procedureName);
-        console.log(' hospitalRegion = ' + hospitalRegion);
+    calculateEstimate(province: string, variety: string, price: number): Observable<Estimate> {
         const httpOptions = {
             headers: new HttpHeaders({
                 'Authorization': 'Bearer ' + this.apiMachineLearningKey,
@@ -30,14 +28,16 @@ export class EstimateService {
             "Inputs": {
               "input1": {
                 "ColumnNames": [
-                  "drg_definition",
-                  "hospital_region",
-                  "avg_total_payments"
+                  "province",
+                  "variety",
+                  "price",
+                  "points"
                 ],
                 "Values": [
                   [
-                    procedureName,
-                    hospitalRegion,
+                    province,
+                    variety,
+                    price,
                     0
                   ]
                 ]
